c88b8124be02e0d9acafa156dad80c6db9b4d0ff
TranLong <tranhophilong12a4@gmail.com>
2023-10-10 15:35:33 +0700

    Delete views: favorite, discover, refresh, personal, Delete file StateSelectionBar.swift

--- DemoSnapkit/MainView/MainViewController.swift
+++ DemoSnapkit/MainView/MainViewController.swift
@@ -10,7 +10,7 @@ import SnapKit
 
 
 
-class MainViewController: UIViewController, SelectionBarDelegate, RefreshViewDelegate{
+class MainViewController: UIViewController{
 
     
     private var contentView = UIView(frame: .zero)
@@ -21,17 +21,15 @@ class MainViewController: UIViewController, SelectionBarDelegate, RefreshViewDel
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        selectionBar.delegate = self
-        let searchView = SearchView()
+        let searchView = ViewForSelectionBar()
         searchView.itemSelectionBar = ItemSelectionBar(title: "Tìm kiếm", img: UIImage(named: "search")!, typeView: .search)
         
-        let refreshView = RefreshView()
-        refreshView.delegate = self
-        refreshView.itemSelectionBar = ItemSelectionBar(title: "", img: UIImage(named: "refresh")!, typeView: .refresh)
+        let centerView = ViewForSelectionBar()
+        centerView.itemSelectionBar = ItemSelectionBar(title: "", img: UIImage(named: "refresh")!, typeView: .refresh)
         
-        let discoverView = DiscoverView()
+        let discoverView = ViewForSelectionBar()
         discoverView.itemSelectionBar = ItemSelectionBar(title: "Khám phá", img: UIImage(named: "discover")!, typeView: .discover)
-        defaultViewControllers = [searchView, refreshView, discoverView]
+        defaultViewControllers = [searchView, centerView, discoverView]
         viewControllers = defaultViewControllers
         
         
@@ -51,15 +49,6 @@ class MainViewController: UIViewController, SelectionBarDelegate, RefreshViewDel
         })
         
         
-        setupBinder()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        navigationController?.navigationBar.isHidden = true
-        viewModel.loadMoreView()
-    
     }
     
 
@@ -67,51 +56,16 @@ class MainViewController: UIViewController, SelectionBarDelegate, RefreshViewDel
         selectionBar.snp.makeConstraints { make in
             make.top.equalTo(self.view.safeAreaLayoutGuide)
             make.centerX.equalToSuperview()
-            make.width.equalTo(self.view.frame.width * 83/100)
+            make.width.equalTo(self.view.frame.width * 65/100)
             make.height.equalTo(50)
         }
     
     }
     
-    
-    func setupBinder(){
-        viewModel.views.bind { [weak self] lstView in
-            var moreViews : [ViewForSelectionBar] = []
-            var moreItems : [ItemSelectionBar] = []
-            
-//         
-            lstView.forEach { view in
-                moreViews.append(view)
-                moreItems.append(view.itemSelectionBar!)
-            }
-            
-            var defaultItem : [ItemSelectionBar] = []
-            
-            self?.defaultViewControllers.forEach { view in
-                defaultItem.append(view.itemSelectionBar!)
-            }
-            
-            self?.viewControllers = self!.defaultViewControllers + moreViews
-            self?.selectionBar.items = defaultItem + moreItems
-
-        }
-    }
-    
-
-    
-    func navtoCustomSelectionBar() {
-        let customSelectionBarView = CustomSelectionBarView()
-
-        self.navigationController?.pushViewController(customSelectionBarView, animated: true)
-    }
-
-    func didSelectedItem(index: Int) {
-        contentView.addSubview(viewControllers![index].view)
-        viewControllers![index].didMove(toParent: self)
-    }
 
 
     
+ 
 }
     
    
--- DemoSnapkit/MainView/MainViewModel.swift
+++ DemoSnapkit/MainView/MainViewModel.swift
@@ -10,26 +10,7 @@ import Foundation
 
 class MainViewModel{
     
-    var views : ObservableObject<[ViewForSelectionBar]> = ObservableObject([])
-    func loadMoreView(){
-        var tempViews : [ ViewForSelectionBar] = []
-        StateSelectionBar.shared._items.forEach { item in
-            if item.isHidden == false{
-                if item.typeView == .favorite{
-                    var favoriteView = FavoriteView()
-                    favoriteView.itemSelectionBar = item
-                    tempViews.append(favoriteView)
-                }else if item.typeView == .peronal{
-                    var personalView = PersonalView()
-                    personalView.itemSelectionBar = item
-                    tempViews.append(personalView)
-                }
-                
-            }
-        }
-        
-        views.value = tempViews
-    }
+
     
     
 }
